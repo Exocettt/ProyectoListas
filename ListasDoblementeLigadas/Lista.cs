d431f5fdc7c8ea90bf08c37fc8de067d9aa4a917
Exocett <jesusadan049@gmail.com>
2023-02-20 13:57:26 -0700

    Otra vez, no se guardaron los cambios sorry.

--- ListasDoblementeLigadas/Lista.cs
+++ ListasDoblementeLigadas/Lista.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,27 @@ namespace ListasDoblementeLigadas
 {
     internal class Lista
     {
+        public Nodo arriba;
+        public Nodo abajo;
+
+        public Lista()
+        {
+            arriba = null;
+            abajo = null;
+        }
+
+        public bool ValidaVacio()
+        {
+           if(arriba==null)
+            {
+                Console.WriteLine("La lista esta vacia");
+            }   
+        }
+
+        public void vaciarLista()
+        {
+            arriba = null;
+            abajo = null;
+        }
     }
 }
