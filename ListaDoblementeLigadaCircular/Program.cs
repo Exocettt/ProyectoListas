d8b0258432b7916b9d790c6e7037807aa67854f2
Exocett <jesusadan049@gmail.com>
2023-02-26 19:04:08 -0700

    clase principal

--- ListaDoblementeLigadaCircular/Program.cs
+++ ListaDoblementeLigadaCircular/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static ListaDoblementeLigadaCircular.Lista;
 
 namespace ListaDoblementeLigadaCircular
 {
@@ -10,6 +11,28 @@ namespace ListaDoblementeLigadaCircular
     {
         static void Main(string[] args)
         {
+            var Lista = new DoublyLinkedList<string>();
+            Console.WriteLine("Lista:");
+            Lista.Agregar("Cuatro");
+            Lista.Agregar("Tres");
+            Lista.Agregar("Dos");
+            Lista.AgregarFinal("Cinco");
+            Lista.Agregar("Uno");
+            Lista.Recorrer();
+            Console.WriteLine("");
+            Console.WriteLine("Buscar Nodo Tres:");
+            Lista.EncontrarNodo("Tres");
+            Console.WriteLine("");
+            Console.WriteLine("Eliminar Ultimo Nodo:");
+            Lista.EliminarFinal();
+            Lista.Recorrer();
+            Console.WriteLine("");
+            Console.WriteLine("Eliminar Primer Nodo:");
+            Lista.EliminarInicio();
+            Lista.Recorrer();
+            
+            
+            Console.ReadKey();
         }
     }
 }
